5bf96b95674080174a0cc43511e0c2b05a23dc4d
ivdeguzman <84778729+ivdeguzman@users.noreply.github.com>
2024-05-03 13:44:28 +0800

    Delete src directory

--- src/stores/imagesSelected.js
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-
-export const switzerlandSelected = writable([0, 0]);
-export const japanSelected = writable([0, 0]);
-export const philippinesSelected = writable([0, 0]);
